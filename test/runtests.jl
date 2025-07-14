b6e819a1727fd3aca34036ef3e4e80d1a07d5dd8
Jimmy <jimbotonic@users.noreply.github.com>
2025-07-14 16:44:03 +0200

    Fibonacci compression OK

--- test/runtests.jl
+++ test/runtests.jl
@@ -657,3 +657,54 @@ end
     # rm(TEST_DIR, force=true, recursive=true)  
 end
 
+@testset "Fibonacci Compression" begin
+    @info("Loading Amazon dataset")
+	amz_g = load_dataset(AMZ_DATASET_IN; separator='\t')
+	# number of vertices and edges
+	@test 403394 == convert(Int,nv(amz_g))
+	@test 3387388 == ne(amz_g)
+	
+	@info("Getting core")
+	amz_core, oni, noi = get_core(amz_g)
+	@test 395234 == convert(Int,nv(amz_core))
+	@test 3301092 == ne(amz_core)
+
+    # create test directory if it doesn't exist
+    mkpath(TEST_DIR)
+    
+    # Use test directory for output files
+    mgs_output_file = joinpath(TEST_DIR, "amz_core")
+
+    encoding_types = [:children, :index]
+    compression_types = [:fibonacci]
+
+    for encoding_type in encoding_types
+        for compression_type in compression_types
+            @info("Writing compressed MGS3 graph with $encoding_type encoding and $compression_type compression")
+            write_compressed_mgs3_graph(amz_core, mgs_output_file * "_" * string(encoding_type) * "_" * string(compression_type), encoding_type, compression_type)
+        end
+    end
+
+    # Test MGS3 format preservation
+    # NB: the output file is created with extension .mgs
+    for encoding_type in encoding_types
+        for compression_type in compression_types
+            @info("Loading compressed MGS3 graph with $encoding_type encoding and $compression_type compression")
+            loaded_graph = load_compressed_mgs3_graph(mgs_output_file * "_" * string(encoding_type) * "_" * string(compression_type) * ".mgz")
+            
+            @info("Verifying graph properties")
+            @test nv(loaded_graph) == nv(amz_core)
+            @test ne(loaded_graph) == ne(amz_core)
+            @test density(loaded_graph) ≈ density(amz_core)
+            @test maximum(outdegree(loaded_graph)) == maximum(outdegree(amz_core))
+            @test minimum(outdegree(loaded_graph)) == minimum(outdegree(amz_core))
+            @test mean(outdegree(loaded_graph)) ≈ mean(outdegree(amz_core))
+            @test median(outdegree(loaded_graph)) ≈ median(outdegree(amz_core))
+            @test std(outdegree(loaded_graph)) ≈ std(outdegree(amz_core))
+        end
+    end
+
+    # clean up the test directory
+    # rm(TEST_DIR, force=true, recursive=true)  
+end
+
